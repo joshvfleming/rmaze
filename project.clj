fc9bf7350be8dfd4e115189c1410b5b890feeaf9
joshvfleming <jf@joshfleming.com>
2012-02-12 21:12:04 -0800

    Added ring adapter dep.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
                  "https://oss.sonatype.org/content/groups/public/"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/data.priority-map "0.0.2-SNAPSHOT"]
+                 [ring/ring-jetty-adapter "1.0.1"]
                  [compojure "0.6.5"]
                  [enlive "1.0.0"]]
   :dev-dependencies [[lein-ring "0.4.5"]
