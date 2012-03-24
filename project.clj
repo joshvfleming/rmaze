c8eaf830d429df95e0eec90c3609a41a30bef7bb
joshvfleming <jf@joshfleming.com>
2012-03-24 11:56:57 -0700

    Updated to clojure 1.4 and leiningen 2.

--- project.clj
+++ project.clj
@@ -2,11 +2,11 @@
   :description "FIXME: write description"
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0-beta4"]
                  [org.clojure/data.priority-map "0.0.2-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [compojure "0.6.5"]
                  [enlive "1.0.0"]
                  [org.clojure/data.json "0.1.2"]]
-  :dev-dependencies [[lein-ring "0.5.4"]]
+  :plugins [[lein-ring "0.6.2"]]
   :ring {:handler rmaze.server/app})
