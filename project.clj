3273f25b54326d13a7982faa3dbda7c4d60fb973
joshvfleming <jf@joshfleming.com>
2012-04-17 07:00:16 -0700

    Updated dep to clojure 1.4 final.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :repositories {"sonatype-oss-public"
                  "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "1.4.0-beta5"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/data.priority-map "0.0.2-SNAPSHOT"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [compojure "0.6.5"]
